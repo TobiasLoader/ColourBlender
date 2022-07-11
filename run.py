2df27163890015ab2ce545a7f7c9308068208e20
Toby <toby@tobias.codes>
2022-07-11 12:38:55 +0100

    Basic stripe integration success

--- run.py
+++ run.py
@@ -1,12 +1,65 @@
-from flask import Flask
-from flask import render_template
+from flask import Flask, render_template, jsonify, request
 from copyright_years import build_copyright_years
+import stripe
+
+# This is your test secret API key.
+stripe.api_key = 'sk_test_51LJzMaAE43t8f8VwHSiFCcHigSL0E4vC6Zvv35z9PJya75YYDcGLsAPRWgbpo5mnoxaoLnNm5fFG7XyYrKOxoTMj00MKqMaqsx'
+
+# stripe.PaymentIntent.create(
+# 	amount=1099,
+# 	currency="eur",
+# 	payment_method_types=[
+# 		"bancontact",
+# 		"card",
+# 		"eps",
+# 		"giropay",
+# 		"ideal",
+# 		"p24",
+# 		"sepa_debit",
+# 		"sofort",
+# 	],
+# )
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
 	return render_template('index.html',copyrightyears=build_copyright_years())
+
+@app.route('/secret')
+def secret():
+	intent = stripe.PaymentIntent.create(
+		amount=30,
+		currency='gbp',
+		payment_method_types=['card'],
+	)
+	return jsonify(client_secret=intent.client_secret)
+		
+# @app.route('/create-payment-intent', methods=['POST'])
+# def create_payment():
+# 	print(request.data)
+# 	try:
+# 		data = json.loads(request.data)
+# 		# Create a PaymentIntent with the order amount and currency
+# 		stripe.PaymentIntent.create(
+# 		  amount=1099,
+# 		  currency="eur",
+# 		  payment_method_types=[
+# 			"bancontact",
+# 			"card",
+# 			"eps",
+# 			"giropay",
+# 			"ideal",
+# 			"p24",
+# 			"sepa_debit",
+# 			"sofort",
+# 		  ],
+# 		)
+# 		return jsonify({
+# 			'clientSecret': intent['client_secret']
+# 		})
+# 	except Exception as e:
+# 		return jsonify(error=str(e))
 
 if __name__ == "__main__":
 	app.run()
---
+++ static/checkout.js
@@ -0,0 +1,47 @@
+// Set your publishable key: remember to change this to your live publishable key in production
+// See your keys here: https://dashboard.stripe.com/apikeys
+const stripe = Stripe('pk_test_51LJzMaAE43t8f8Vwd9V4opGsnZxhD2h2AzmFEZpXbyXYtmDuQaYXANvtAaxymr1R7eabRUmMHEzMb7or3ddFnTyh00YfCKuCOg');
+
+var response = fetch('/secret').then(function(response) {
+  return response.json();
+}).then(function(responseJson) {
+  var clientSecret = responseJson.client_secret;
+  
+  const options = {
+	clientSecret: clientSecret,
+  };
+  
+  // Set up Stripe.js and Elements to use in checkout form, passing the client secret obtained in step 2
+  const elements = stripe.elements(options);
+  
+  // Create and mount the Payment Element
+  const paymentElement = elements.create('payment');
+  paymentElement.mount('#payment-element');
+
+  // Call stripe.confirmCardPayment() with the client secret.
+  const form = document.getElementById('payment-form');
+  
+  form.addEventListener('submit', async (event) => {
+	event.preventDefault();
+  
+	const {error} = await stripe.confirmPayment({
+	  //`Elements` instance that was used to create the Payment Element
+	  elements,
+	  confirmParams: {
+		return_url: 'http://localhost:5000/?payment_intent_client_secret='+clientSecret,
+	  },
+	});
+  
+	if (error) {
+	  // This point will only be reached if there is an immediate error when
+	  // confirming the payment. Show error to your customer (for example, payment
+	  // details incomplete)
+	  const messageContainer = document.querySelector('#error-message');
+	  messageContainer.textContent = error.message;
+	} else {
+	  // Your customer will be redirected to your `return_url`. For some payment
+	  // methods like iDEAL, your customer will be redirected to an intermediate
+	  // site first to authorize the payment, then redirected to the `return_url`.
+	}
+  });
+});
