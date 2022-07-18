d67915c6cd113f88a44b9525fb31d5a2028d9932
Toby <toby@tobias.codes>
2022-07-18 18:12:18 +0100

    payment display tweaks

--- static/js/status.js
+++ static/js/status.js
@@ -18,14 +18,17 @@ if (clientSecret!=null) {
 		// [0]: https://stripe.com/docs/payments/payment-methods#payment-notification
 		switch (paymentIntent.status) {
 			case 'succeeded':
-  				message.html('Success! Payment of £'+parseFloat(paymentIntent.amount/100).toFixed(2).toString()+' received.<br><br>Click <a href="http://localhost:5000">here</a> to return to the home page.');
+  				message.html('Success! Payment of £'+parseFloat(paymentIntent.amount/100).toFixed(2).toString()+' received.<br><br>Click <a href="'+window.location.origin+'">here</a> to return to the home page.');
 					$('#choose-amount-section').css('display','none');
+					$('#donate-content > h2').text("Thank-you for tipping! You're amazing :)");
+					$('#donate-content > .desc').text("With your help I'll be able to fuel myself to build more great apps for you to use & enjoy.");
   				break;
 			case 'processing':
   				message.text("Payment processing. We'll update you when payment is received.");
   				break;
 			case 'requires_payment_method':
-  				message.text('Payment failed. Please try another payment method.');
+  				message.text('Payment failed. Please try another payment method.<br><br>Click <a href="'+window.location.origin+'/coffee">here</a> to try again.');
+				  $('#choose-amount-section').css('display','none');
   				// Redirect your user back to your payment page to attempt collecting
   				// payment again
   				break;
